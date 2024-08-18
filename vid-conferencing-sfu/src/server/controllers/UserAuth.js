11645c9b9ef93638f04b6a5a695570d1f9e6cf4c
Anuj Dubey <anujd2408@gmail.com>
2024-08-18 20:14:35 +0530

    Adding backend files

--- vid-conferencing-sfu/src/server/controllers/UserAuth.js
+++ vid-conferencing-sfu/src/server/controllers/UserAuth.js
@@ -1,1 +1,19 @@
 //models check info using zod
+const zod = require("zod")
+const signupBody = zod.object({
+    username: zod.string().email(),
+	firstName: zod.string().max(50).trim(),
+	lastName: zod.string().max(50).trim(),
+	password: zod.string().min(6)
+})
+
+const signinBody = zod.object({
+    username: zod.string().email(),
+	password: zod.string()
+})
+
+const updateBody = zod.object({
+	password: zod.string().optional(),
+    firstName: zod.string().optional(),
+    lastName: zod.string().optional(),
+})
